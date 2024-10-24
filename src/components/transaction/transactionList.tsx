93eb6c8441c0a3ac144cfb42df0abf95360de08b
Izan Mata <matadiazizan@gmail.com>
2024-10-24 18:00:00 +0200

    Transaction with interface in api response

--- src/components/transaction/transactionList.tsx
+++ src/components/transaction/transactionList.tsx
@@ -1,18 +1,35 @@
 import Box from "@mui/material/Box";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { Transaction } from "../../models";
+import { getCategoryById } from "../../services/categoryService";
 
 interface TransactionListProps {
-    transaction_list: Transaction[];
+  transaction_list: Transaction[];
 }
 
-export default function TransactionList({transaction_list} : TransactionListProps) {
-    return (
-        <Box sx={{ height: 250, width: "100%" }}>
-            <DataGrid
-                columns={[{ field: "datetime" }, { field: "amount" }, { field: "description" }]}
-                rows={transaction_list}
-            />
-        </Box>
-    );
+export default function TransactionList({
+  transaction_list,
+}: TransactionListProps) {
+  const columns: GridColDef[] = [
+    { field: "datetime", headerName: "Fecha y Hora", width: 150 },
+    { field: "amount", headerName: "Monto", width: 100 },
+    { field: "description", headerName: "Descripción", width: 200 },
+    { field: "category_name", headerName: "Categoría", width: 150 },
+  ];
+
+  const rows = transaction_list.map((transaction, index) => {
+    return {
+      id: index,
+      datetime: transaction.datetime,
+      amount: transaction.amount,
+      description: transaction.description,
+      category_name: transaction.category.name,
+    };
+  });
+
+  return (
+    <Box sx={{ height: 250, width: "100%" }}>
+      <DataGrid columns={columns} rows={rows} />
+    </Box>
+  );
 }
--- src/models.ts
+++ src/models.ts
@@ -1,31 +1,30 @@
-
 export interface Account {
-    id?:     number;
-    amount: number;
-    user:   number;
+  id?: number;
+  amount: number;
+  user: number;
 }
 
 export interface Category {
-    id?:          number;
-    name:        string;
-    description: string;
+  id?: number;
+  name: string;
+  description: string;
 }
 
 export interface Transaction {
-    id?:          number;
-    type:        string;
-    datetime:    Date;
-    amount:      number;
-    description: string;
-    account:     number;
-    category:    number;
+  id?: number;
+  type: string;
+  datetime: Date;
+  amount: number;
+  description: string;
+  account: number;
+  category: Category;
 }
 
 export interface Expense {
-    id?:           number;
-    description:  string;
-    expense_type: string;
-    amount:       number;
-    deadline:     null;
-    category:     number;
+  id?: number;
+  description: string;
+  expense_type: string;
+  amount: number;
+  deadline: null;
+  category: number;
 }
